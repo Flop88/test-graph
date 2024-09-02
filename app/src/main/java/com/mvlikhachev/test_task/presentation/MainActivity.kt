df8a5ef8409a5968cf4429283820b08a93aa23d5
MV.Likhachev <m.lihachev@paysend.com>
2024-09-02 20:43:33 +0200

    Update Readme

--- app/src/main/java/com/mvlikhachev/test_task/presentation/MainActivity.kt
+++ app/src/main/java/com/mvlikhachev/test_task/presentation/MainActivity.kt
@@ -8,15 +8,6 @@ import androidx.core.view.WindowInsetsCompat
 import com.mvlikhachev.test_task.R
 import dagger.hilt.android.AndroidEntryPoint
 
-
-/**
- * Можно сделать конечно лучше, но времени честно говоря нет.
- * Можно разбить на модуль, написать тесты, текст вынести в ресурсы и т.п
- * Кастомные вьюхи тоже очень-очень давно не делал и накидал эту "по-быстрому"
- * Ну и UI красивым тоже не делал, а сделал просто чтоб работало. Дальше можно кастомизировать
- * как дизайнеры захотят
- **/
-
 @AndroidEntryPoint
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
